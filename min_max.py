24883ce31b726c4f6895146cbaea727885db0a35
morca1 <155597699+morca1@users.noreply.github.com>
2024-02-18 09:49:52 -0800

    Add files via upload

---
+++ min_max.py
@@ -0,0 +1,10 @@
+# Example list of numbers
+numbers = [3, 4, 1, 5, 6, 2]
+
+# Find and print the minimum value
+minimum = min(numbers)
+print("Minimum value:", minimum)
+
+# Find and print the maximum value
+maximum = max(numbers)
+print("Maximum value:", maximum)
